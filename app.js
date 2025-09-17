246bd8df41ddc40e528c4fdf155d3bc40efa89bf
hej6 <hej6@southernct.edu>
2025-09-17 16:24:55 +0000

    Added checks

--- app.js
+++ app.js
@@ -37,32 +37,38 @@ let serverObj = http.createServer(function (req, res)
 
 function schedule(queryObj, res)
 {
-	if (!availableTimes[queryObj.day])
+	if (!queryObj.name || !queryObj.time || !queryObj.day) //Checks for missing variables
 	{
-		return error(400, 'Day not recognized', res)
+		return error (400, 'Missing information', res);
+	}
+
+	if (!availableTimes[queryObj.day]) //Checks for day specifically
+	{
+		return error(400, 'Requested day is invalid', res);
 	}
 	
 	const index = availableTimes[queryObj.day].indexOf(queryObj.time); //Stores requested time slot
 
-	if (index !== -1)
+	if (index === -1) //Checks time slot
 	{
-		availableTimes[queryObj.day].splice(index, 1); //Remove time slot
-		
-		const newAppt = //Initializes and populates newAppt
-		{
-			name: queryObj.name,
-			day: queryObj.day,
-			time: queryObj.time
-		};
-		appointments.push(newAppt); //Adds newAppt to array
-		console.log('List of appointments: ', appointments); //Display appointments array
-
-		res.writeHead(200, {'content-type': 'text/html'});
-                res.write('Scheduled');
-		res.end();
+		return error(400, 'Requested time slot is invalid', res);	
 	}
-	else
-		error(400, 'Cannot schedule', res);
+	
+	//Run code if checks pass
+	availableTimes[queryObj.day].splice(index, 1); //Remove time slot
+
+	const newAppt = //Initializes and populates newAppt
+        {
+        	name: queryObj.name,
+                day: queryObj.day,
+                time: queryObj.time
+	};
+       	appointments.push(newAppt); //Adds newAppt to array
+        console.log('List of appointments: ', appointments); //Check in console
+
+       	res.writeHead(200, {'content-type': 'text/html'});
+       	res.write('Appointment has been scheduled');
+        res.end();
 }
 
 function cancel()
